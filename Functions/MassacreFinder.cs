c2d95cc86db613f3bc48a8c6c85a95a695daf70e
Zachary Austin <Zachary@admiralfeb.net>
2022-02-04 20:50:55 -0600

    feat: add init massacrefinder

---
+++ Functions/MassacreFinder.cs
@@ -0,0 +1,11 @@
+using EDCalculations.EDSM;
+
+namespace EDCalculations.Functions;
+
+public static class MassacreFinder
+{
+    public static async Task FindMassacreSystemAsync(string systemName, int distance)
+    {
+        throw new NotImplementedException();
+    }
+}
