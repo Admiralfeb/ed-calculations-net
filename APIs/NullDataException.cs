5787163b4278da5bc806524c9c3c4494252f41c8
Zachary Austin <Zachary@admiralfeb.net>
2022-02-07 20:21:44 -0600

    fix: nulldataexception sonar smell

--- APIs/NullDataException.cs
+++ APIs/NullDataException.cs
@@ -1,6 +1,11 @@
+using System.Runtime.Serialization;
+
 namespace EDCalculations.APIs;
 
+[Serializable]
 public sealed class NullDataException : Exception
 {
     public NullDataException(string? message) : base(message) { }
+
+    private NullDataException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 }
